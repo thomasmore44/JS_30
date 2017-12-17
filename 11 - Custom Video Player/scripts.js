bdbc61fcf5e30e0c7bb01e16bc4850ed49da92f5
Tom C <tcox44@gmail.com>
2017-12-17 15:04:19 -0500

    Halfway through HTML Video Project

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,41 @@
+/* Grab Elements */
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+
+
+
+
+/* Write Functions */
+function togglePlay() {
+  const method = video.paused ? 'play' : 'pause';
+  video[method]();
+
+  // if (video.paused) {
+  //   video.play();
+  // }
+  // else {
+  //   video.pause();
+  // }
+}
+
+function updateButton() {
+  const icon = this.paused ? '►' : '❚ ❚';
+  toggle.textContent = icon;
+}
+
+
+
+/* Set Up Event Listeners */
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
+toggle.addEventListener('click', togglePlay);
+skipButtons.forEach()
