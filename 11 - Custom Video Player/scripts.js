040f6e46a87405cff362d345448930e45532a541
Tom C <tcox44@gmail.com>
2017-12-17 16:20:02 -0500

    still trying to figure out fullscreen toggle

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -7,7 +7,7 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
-
+const fullScreenButton = player.querySelector('.full-screen');
 
 
 
@@ -30,12 +30,48 @@ function updateButton() {
   toggle.textContent = icon;
 }
 
+function skip() {
+  // console.log(this.dataset.skip);
+  video.currentTime += parseFloat(this.dataset.skip);
+}
 
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+  console.log(e);
+}
+
+function toggleFullScreen() {
+  if (video.requestFullscreen) {
+    video.requestFullscreen();
+  } else {
+    document.exitFullscreen();
+  }
+}
 
 /* Set Up Event Listeners */
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+fullScreenButton.addEventListener('click', toggleFullScreen);
+
 
 toggle.addEventListener('click', togglePlay);
-skipButtons.forEach()
+skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+
+let mousedown = false;
+progress.addEventListener('click', scrub);
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
